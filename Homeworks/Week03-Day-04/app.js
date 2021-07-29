ae1ff977534a123f09a3ca09e4938bc63343aeba
Huseyn Mammadov <huseynmv@yahoo.com>
2021-07-29 21:03:51 +0400

    Presentation codes examples

--- Homeworks/Week03-Day-04/app.js
+++ Homeworks/Week03-Day-04/app.js
@@ -1,28 +1,82 @@
-// numeric string used with + gives string type
-let result;
-
-result = '3' + 2; 
-console.log(result) // "32"
-
-result = '3' + true; 
-console.log(result); // "3true"
-
-result = '3' + undefined; 
-console.log(result); // "3undefined"
-
-result = '3' + null; 
-console.log(result); // "3null"
-
+// non-numeric string used with - , / , * results to NaN
 
 let result;
 
-// string to number
-result = Number('324');
-console.log(result); // 324
+result = 'hello' - 'world';
+console.log(result); // NaN
 
-// boolean to number
-result = Number(true);
-console.log(result); // 1
+result = '4' - 'hello';
+console.log(result); // NaN
 
-result = Number(false);
-console.log(result); // 0
+
+
+// if boolean is used, true is 1, false is 0
+
+let result;
+
+result = '4' - true;
+console.log(result); // 3
+
+result = 4 + true;
+console.log(result); // 5
+
+result = 4 + false;
+console.log(result); // 4
+
+
+
+
+
+
+let result;
+result = Boolean('');
+console.log(result); // false
+
+result = Boolean(0);
+console.log(result); // false
+
+result = Boolean(undefined);
+console.log(result); // false
+
+result = Boolean(null);
+console.log(result); // false
+
+result = Boolean(NaN);
+console.log(result); // false
+
+result = Boolean(324);
+console.log(result); // true
+
+result = Boolean('hello');
+console.log(result); // true
+
+result = Boolean(' ');
+console.log(result); // true
+
+
+
+
+
+//number to string
+let result;
+result = String(324);
+console.log(result);  // "324"
+
+result = String(2 + 4);
+console.log(result); // "6"
+
+//other data types to string
+result = String(null);
+console.log(result); // "null"
+
+result = String(undefined);
+console.log(result); // "undefined"
+
+result = String(NaN);
+console.log(result); // "NaN"
+
+result = String(true);
+console.log(result); // "true"
+
+result = String(false);
+console.log(result); // "false"

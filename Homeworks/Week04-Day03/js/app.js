c2d3c9b3ed68e080e47149a83d1735080a1cbc50
Huseyn Mammadov <huseynmv@yahoo.com>
2021-08-04 16:22:08 +0400

    box movement updated

--- Homeworks/Week04-Day03/js/app.js
+++ Homeworks/Week04-Day03/js/app.js
@@ -3,21 +3,25 @@ let up=document.querySelector('#up')
 let down=document.querySelector('#down')
 let right=document.querySelector('#right')
 let left=document.querySelector('#left')
-
+marginAmount=0
 
 up.addEventListener('click', function(){
-    box.style.marginBottom='20px'
+    box.style.marginBottom=`${marginAmount}px`
+    marginAmount+=20
 })
 
 down.addEventListener('click', function(){
-    box.style.marginTop='20px'
+    box.style.marginTop=`${marginAmount}px`
+    marginAmount+=20
 })
 
 right.addEventListener('click', function(){
-    box.style.marginLeft='20px'
+    box.style.marginLeft=`${marginAmount}px`
+    marginAmount+=20
 })
 
 left.addEventListener('click', function test(){
-    box.style.marginRight='20px'
+    box.style.marginRight=`${marginAmount}px`
+    marginAmount+=20
 })
 

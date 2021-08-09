8d0b3deef8b7dbbbbbc4b766f8a842e3b4ddd033
Huseyn Mammadov <huseynmv@yahoo.com>
2021-08-09 21:58:16 +0400

    About me page full responsibility completed

--- Portfolio/js/app.js
+++ Portfolio/js/app.js
@@ -7,12 +7,12 @@ let ui = document.querySelector('.ui-container')
 let marketing = document.querySelector('.marketing-container')
 let ecommerce = document.querySelector('.ecommerce-container')
 let copyright = document.querySelector('.copyright-container')
-// let heci = document.querySelector('.heci')
+let heci = document.querySelector('.heci')
 window.addEventListener('scroll', ()=>{
-    ui.classList.toggle('animation', window.scrollY>200)
-    marketing.classList.toggle('animation', window.scrollY>200)
-    ecommerce.classList.toggle('animation', window.scrollY>200)
-    copyright.classList.toggle('animation', window.scrollY>200)
+    ui.classList.toggle('animation', window.scrollY>100)
+    marketing.classList.toggle('animation', window.scrollY>100)
+    ecommerce.classList.toggle('animation', window.scrollY>100)
+    copyright.classList.toggle('animation', window.scrollY>100)
 })
 
 // window.addEventListener('scroll', ()=>{
